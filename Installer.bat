bc2ec0c17a382d323110ff7de12ec3db22314f7d
RoboXL <105053104+RoboXL@users.noreply.github.com>
2023-05-13 10:06:40 -0400

    Update Installer.bat

--- Installer.bat
+++ Installer.bat
@@ -3,12 +3,15 @@
 REM Define variables
 set "repoUrl=https://github.com/RoboXL/UpdateX-Windows/archive/refs/heads/main.zip"
 set "tempDir=%TEMP%"
-set "downloadPath=%tempDir%\UpdateX-Windows.zip"
-set "extractPath=%tempDir%\UpdateX-Windows"
-set "scriptPath=%extractPath%\UpdateX-Windows-main\UpdateX-Windows.bat"
+set "downloadPath=%tempDir%\UpdateX.zip"
+set "extractPath=%tempDir%\UpdateX"
+set "scriptPath=%extractPath%\UpdateX-Windows-main\UpdateX.bat"
+set "updateScriptPath=%extractPath%\UpdateX-Windows-main\Update UpdateX.bat"
 set "destinationFolder=%USERPROFILE%\UpdateX"
-set "destinationPath=%destinationFolder%\UpdateX-Windows.bat"
-set "shortcutPath=%APPDATA%\Microsoft\Windows\Start Menu\Programs\UpdateX-Windows.lnk"
+set "destinationPath=%destinationFolder%\UpdateX.bat"
+set "updateScriptDestinationPath=%destinationFolder%\Update UpdateX.bat"
+set "shortcutPath=%APPDATA%\Microsoft\Windows\Start Menu\Programs\UpdateX.lnk"
+set "updateShortcutPath=%APPDATA%\Microsoft\Windows\Start Menu\Programs\Update UpdateX.lnk"
 
 REM Prompt to confirm installation
 echo UpdateX-Windows installation
@@ -30,11 +33,15 @@ powershell.exe -Command "Expand-Archive -Path '%downloadPath%' -DestinationPath 
 REM Create the destination folder if it doesn't exist
 mkdir "%destinationFolder%" 2>nul
 
-REM Move the script to the desired location
+REM Move the scripts to the desired locations
 move /Y "%scriptPath%" "%destinationPath%"
+move /Y "%updateScriptPath%" "%updateScriptDestinationPath%"
 
-REM Create a shortcut in the Start Menu folder
+REM Create a shortcut in the Start Menu folder for UpdateX-Windows
 powershell.exe -Command "$shell = New-Object -ComObject WScript.Shell; $shortcut = $shell.CreateShortcut('%shortcutPath%'); $shortcut.TargetPath = '%destinationPath%'; $shortcut.Save()"
+
+REM Create a shortcut in the Start Menu folder for Update UpdateX.bat
+powershell.exe -Command "$shell = New-Object -ComObject WScript.Shell; $shortcut = $shell.CreateShortcut('%updateShortcutPath%'); $shortcut.TargetPath = '%updateScriptDestinationPath%'; $shortcut.Save()"
 
 REM Clean up temporary files
 del "%downloadPath%" /F /Q
@@ -42,5 +49,9 @@ rmdir "%extractPath%" /S /Q
 
 echo.
 echo UpdateX-Windows installation completed.
+
+echo UpdateX completed.
 echo.
 pause
+
+
