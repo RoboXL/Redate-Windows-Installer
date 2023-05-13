0c5e243df2ffde2e3686b54e26d00deb8e376f80
RoboXL <105053104+RoboXL@users.noreply.github.com>
2023-05-12 21:23:28 -0400

    Update Installer.bat

--- Installer.bat
+++ Installer.bat
@@ -10,6 +10,17 @@ set "destinationFolder=%USERPROFILE%\UpdateX"
 set "destinationPath=%destinationFolder%\UpdateX-Windows.bat"
 set "shortcutPath=%APPDATA%\Microsoft\Windows\Start Menu\Programs\UpdateX-Windows.lnk"
 
+REM Prompt to confirm installation
+echo UpdateX-Windows installation
+echo Run this again to update UpdateX
+echo.
+echo This script will install UpdateX-Windows to the following location:
+echo  %destinationFolder%
+echo.
+
+set /p "choice=Do you want to continue with the installation? (Y/N): "
+if /i "%choice%" neq "Y" exit
+
 REM Download the repository zip file
 powershell.exe -Command "(New-Object System.Net.WebClient).DownloadFile('%repoUrl%', '%downloadPath%')"
 
@@ -25,13 +36,11 @@ move /Y "%scriptPath%" "%destinationPath%"
 REM Create a shortcut in the Start Menu folder
 powershell.exe -Command "$shell = New-Object -ComObject WScript.Shell; $shortcut = $shell.CreateShortcut('%shortcutPath%'); $shortcut.TargetPath = '%destinationPath%'; $shortcut.Save()"
 
-REM Create a folder in the user's Startup folder
-mkdir "%startupFolder%" 2>nul
-
 REM Clean up temporary files
 del "%downloadPath%" /F /Q
 rmdir "%extractPath%" /S /Q
 
+echo.
 echo UpdateX-Windows installation completed.
-
+echo.
 pause
